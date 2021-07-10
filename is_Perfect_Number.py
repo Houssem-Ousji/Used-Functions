8dc999761f3379b68fbe46ba1edea67a61c9daed
HOUSSEM <ousjihoussem01@gmail.com>
2021-07-10 03:25:03 +0200

    test Your number

---
+++ is_Perfect_Number.py
@@ -0,0 +1,16 @@
+def is_Perfect_Number(value):
+    dividers = 0
+    for i in range(1, (value//2)+1):
+        if value % i == 0:
+            dividers += i
+    if dividers == value:
+        return True
+    else:
+        return False
+print(is_Perfect_Number(1))
+#You got : False
+print(is_Perfect_Number(6))
+#You got : True
+print(is_Perfect_Number(8128))
+#You got : True
+
