59302e7feaf1e055e397ee829f0cfdad9daeac40
HOUSSEM <ousjihoussem01@gmail.com>
2021-07-18 17:27:35 +0200

    test your number

---
+++ is_Odd_Number.py
@@ -0,0 +1,10 @@
+def is_Odd_Number(value):
+    if value % 2 != 0:
+        return True
+    else:
+        return False
+
+print(is_Odd_Number(2))
+# return False
+print(is_Odd_Number(55))
+# return True
