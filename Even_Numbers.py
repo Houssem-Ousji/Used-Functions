486794ca918da3e39a0e891768b9658ad135ab62
HOUSSEM <ousjihoussem01@gmail.com>
2021-07-18 17:26:28 +0200

    take your list

--- Even_Numbers.py
+++ Even_Numbers.py
@@ -10,3 +10,4 @@ def Even_Numbers(ivalue, fvalue):
             Even_Num.append(i)
     return Even_Num
 print(Even_Numbers(20, 50))
+# return [20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50]
