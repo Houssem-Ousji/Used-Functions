db62d4bd594eb2c157cedc2c7733ede6ee363289
HOUSSEM <ousjihoussem01@gmail.com>
2021-07-18 17:19:41 +0200

    test your number

---
+++ is_Even_Number.py
@@ -0,0 +1,9 @@
+def is_Even_Number(value):
+    if value % 2 == 0:
+        return True
+    else:
+        return False
+print(is_Even_Number(2))
+# return True
+print(is_Even_Number(11))
+# return False
